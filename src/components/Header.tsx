2775b626955554fd270c9287fc3145c0a227097e
[dyad] <pastorinimatheus@gmail.com>
2025-10-21 02:15:39 -0300

    [dyad] wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import { NotificationCenter } from "./NotificationCenter";
 import { ThemeToggle } from "./ThemeToggle";
+import { usePageActions } from "@/contexts/PageActionsContext";
+import React from "react";
 
 export function Header() {
+  const { pageActions } = usePageActions();
+
   return (
-    <div className="absolute top-6 right-6 lg:top-8 lg:right-8 z-10 flex items-center gap-4">
+    <div className="absolute top-6 right-6 lg:top-8 lg:right-8 z-10 flex flex-nowrap items-center gap-4 max-w-full">
+      {pageActions}
       <NotificationCenter />
       <ThemeToggle />
     </div>
