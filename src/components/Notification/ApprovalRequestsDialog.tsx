a97ef40bf6e57d0d96dd9826f49706a9dda1cfc4
[dyad] <pastorinimatheus@gmail.com>
2025-10-22 18:13:23 -0300

    [dyad] Ajustando a query de busca de solicitações de aprovação para resolver relações de perfil - wrote 1 file(s)

--- src/components/Notification/ApprovalRequestsDialog.tsx
+++ src/components/Notification/ApprovalRequestsDialog.tsx
@@ -28,7 +28,8 @@ async function fetchPendingApprovalRequests(userRole: UserRole): Promise<Approva
     .from("approval_requests")
     .select(`
       *,
-      requester:profiles(first_name, last_name, role),
+      // Usando a coluna user_id como a FK para a tabela profiles
+      requester:profiles!user_id(first_name, last_name, role),
       mesa:mesas!mesa_id_fk(numero),
       item_pedido:itens_pedido!item_pedido_id_fk(id, nome_produto, quantidade, preco, desconto_percentual, desconto_motivo)
     `)
