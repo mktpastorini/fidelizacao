3aa2f4a568ad8c36c7d792d40c667f041df3cd1c
[dyad] <pastorinimatheus@gmail.com>
2025-10-23 19:15:18 -0300

    [dyad] Implementing global splash cursor effect using WebGL - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,9 @@ import NotFound from "./pages/NotFound";
 import MenuPublicoPage from "./pages/MenuPublico";
 import UsuariosPage from "./pages/Usuarios";
 import CozinheirosPage from "./pages/Cozinheiros";
-import GorjetasPage from "./pages/Gorjetas"; // Importado
+import GorjetasPage from "./pages/Gorjetas";
 import { PageActionsProvider } from "./contexts/PageActionsContext";
+import { SplashCursor } from "./components/SplashCursor"; // Importado
 
 const queryClient = new QueryClient();
 
@@ -32,6 +33,23 @@ const App = () => (
       <TooltipProvider>
         <Sonner />
         <BrowserRouter>
+          {/* SplashCursor aplicado globalmente */}
+          <SplashCursor
+            SIM_RESOLUTION={128}
+            DYE_RESOLUTION={1440}
+            CAPTURE_RESOLUTION={512}
+            DENSITY_DISSIPATION={3.5}
+            VELOCITY_DISSIPATION={2}
+            PRESSURE={0.1}
+            PRESSURE_ITERATIONS={20}
+            CURL={3}
+            SPLAT_RADIUS={0.2}
+            SPLAT_FORCE={6000}
+            SHADING={true}
+            COLOR_UPDATE_SPEED={10}
+            BACK_COLOR={{ r: 0.5, g: 0, b: 0 }}
+            TRANSPARENT={true}
+          />
           <Routes>
             {/* Rota Pública para o Menu, agora envolvida por SettingsProvider */}
             <Route path="/menu-publico/:mesaId" element={
@@ -71,7 +89,7 @@ const App = () => (
                 
                 {/* Restringindo acesso a Gerência */}
                 <Route path="/historico" element={<RoleGuard allowedRoles={['superadmin', 'admin', 'gerente']}><Historico /></RoleGuard>} />
-                <Route path="/gorjetas" element={<RoleGuard allowedRoles={['superadmin', 'admin', 'gerente', 'garcom']}><GorjetasPage /></RoleGuard>} /> {/* ATUALIZADO */}
+                <Route path="/gorjetas" element={<RoleGuard allowedRoles={['superadmin', 'admin', 'gerente', 'garcom']}><GorjetasPage /></RoleGuard>} />
                 <Route path="/mensagens" element={<RoleGuard allowedRoles={['superadmin', 'admin', 'gerente']}><Mensagens /></RoleGuard>} />
                 
                 {/* Configurações: Apenas Superadmin e Admin */}
